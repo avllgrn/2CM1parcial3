360c4b34b3ca96d3db395b4a7e996979194ce56e
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-05-24 16:53:45 -0600

    Matriz Transpuesta

--- ejemplo.py
+++ ejemplo.py
@@ -1,20 +1,30 @@
 from os import system
+from random import randrange 
 
 if __name__ == '__main__':
     system('cls')
     
+    m = int(input('Dame m '))
     n = int(input('Dame n '))
-    
-    M = [ [0 for j in range(n)] for i in range(n)]
 
-    for i in range(n):
+    mT = n
+    nT = m
+
+    M = [ [randrange(11) for j in range(n)] for i in range(m)]
+    MT = [ [0 for j in range(nT)] for i in range(mT)]
+
+    for i in range(m):
         for j in range(n):
-            if i==j:
-                M[i][j] = 1
-            else:
-                M[i][j] = 0
+            MT[j][i] = M[i][j]
 
-    for i in range(n):
+    print('\nM')
+    for i in range(m):
         for j in range(n):
             print(f'{M[i][j]}', end='\t')
+        print()
+
+    print('\nMT')
+    for i in range(mT):
+        for j in range(nT):
+            print(f'{MT[i][j]}', end='\t')
         print()
