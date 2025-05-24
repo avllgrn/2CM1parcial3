ca6d288f3f86d5121dcd18cc8fa2e1cfce5ea7e1
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-05-24 16:53:07 -0600

    Matriz Identidad

--- ejemplo.py
+++ ejemplo.py
@@ -3,13 +3,18 @@ from os import system
 if __name__ == '__main__':
     system('cls')
     
-    m = int(input('Dame m '))
     n = int(input('Dame n '))
     
-    M = [ [0 for j in range(n)] for i in range(m)]
-    print(M,'\n\n')
+    M = [ [0 for j in range(n)] for i in range(n)]
 
-    for i in range(m):
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                M[i][j] = 1
+            else:
+                M[i][j] = 0
+
+    for i in range(n):
         for j in range(n):
             print(f'{M[i][j]}', end='\t')
         print()
