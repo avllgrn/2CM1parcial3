3ed9cb05adea3e763ff869085bd602f6a488800d
avllgrn <avillagran@ipn.mx>
2025-05-26 12:31:59 -0600

    Rellenar matriz de Arriba a Abajo, de Derecha a Izquierda

--- ejemplo.py
+++ ejemplo.py
@@ -6,11 +6,11 @@ def muestraMatriz(X,m,n):
             print(X[i][j],end='\t')
         print()
 
-def matrizIzqDerArrAba(m, n):
+def matrizArrAbaIzqDer(m, n):
     X = [[0 for j in range(n)] for i in range(m)]
     cont = 1
-    for i in range(m):
-        for j in range(n-1, -1, -1):
+    for j in range(n-1, -1, -1):
+        for i in range(m):
             X[i][j] = cont
             cont += 1
     return X
@@ -18,10 +18,10 @@ def matrizIzqDerArrAba(m, n):
 if __name__ == '__main__':
     system('cls')
 
-    print('Rellenar de Derecha a Izquierda, Arriba a Abajo\n')
+    print('Rellenar de Arriba a Abajo, Derecha a Izquierda\n')
     m = int(input('Dame m '))
     n = int(input('Dame n '))
-    M4 = matrizIzqDerArrAba(m, n)
+    M4 = matrizArrAbaIzqDer(m, n)
     print('M4')
     muestraMatriz(M4, m, n)
     print('\n\n')
